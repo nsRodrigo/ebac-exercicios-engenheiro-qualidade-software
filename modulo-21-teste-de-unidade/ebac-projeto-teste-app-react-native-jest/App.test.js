f97695848799d5035b3fc63c2e640927c9dd5a7d
nsRodrigo <63819788+nsRodrigo@users.noreply.github.com>
2024-02-18 18:02:42 -0300

    Delete modulo-21-teste-de-unidade directory

--- modulo-21-teste-de-unidade/ebac-projeto-teste-app-react-native-jest/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import App from "./App"
-
-test('tela é renderizada corretamente', () => {
-    const tree = renderer.create(<App />).toJSON()
-    expect(tree)
-});
